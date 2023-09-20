f7393ef056ef6540de1ac175af681d0d8fe8e5ba
Andre Burgaud <andre.burgaud@gmail.com>
2023-09-20 00:35:20 -0500

    bumped version for dev

--- src/jwt/common.nim
+++ src/jwt/common.nim
@@ -1,7 +1,7 @@
 const
-  ver* = "0.9.0"
+  ver* = "0.10.0"
     ## Application version
-  suffix* = ""
+  suffix* = "-dev"
     ## Version suffix (empty for a release version)
   name* = "JWT Command Line"
     ## Command description
